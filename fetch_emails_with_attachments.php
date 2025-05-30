6cb1c84f18506c00a6a85a71bd2dae2b245236e7
Meng <lumeng.lim@gmail.com>
2025-05-30 15:52:41 +0800

    Update helpdesk view and fetch script for clean body rendering

--- fetch_emails_with_attachments.php
+++ fetch_emails_with_attachments.php
@@ -1,6 +1,8 @@
 <?php
 
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Email config
 $hostname = '{mail.oojeema.com:993/imap/ssl}INBOX';
 $username = 'support@oojeema.com';
